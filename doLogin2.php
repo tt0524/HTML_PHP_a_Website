57b1128e069746279d5f69cae587b04b93599918
Xiaoting Jin <xajin@linkedin.com>
2019-04-09 23:08:05 -0700

    lab5

--- doLogin2.php
+++ doLogin2.php
@@ -1,10 +1,10 @@
 <?php
 header("Content-type:text/html;charset=UTF-8");
 // require "mysql.php";            //import mysql.php
+unset($_SESSION['username']);
 session_start();                //new session
 $username=$_POST['username'];
 $password=$_POST['password'];
-$autologin=isset($_POST['autologin'])?1:0;     // achieve auto login
 
 // default username & password
 $GLOBALS['MY_username'] = "admin";
@@ -16,15 +16,7 @@ $GLOBALS['MY_password'] = "12345678";
  * */
 if(checkEmpty($username,$password)){
     if(checkUser($username,$password)){
-        $_SESSION['username']=$username;  //save user name
-        if($autologin==1){                //if choose autologin, save cookies
-            setcookie("username",$username,time()+3600*24*3);   //cookies valid date: 3 days
-            setcookie("password",md5($password),time()+3600*24*3);
-        }
-        else{
-            setcookie("username","",time()-1);   //if no auto login, clear cookie
-            setcookie("password","",time()-1);
-        }
+        $_SESSION['username']=$username;            //save user name
         header("location: users.html ");            //if verified
     }
 }
@@ -76,52 +68,3 @@ function fieldBlank() {
     echo '<html><head><Script Language="JavaScript">alert("Username or Password blank!");</Script></head></html>' . 
     "<meta http-equiv=\"refresh\" content=\"0;url=login.html\">";
 }
-
-
-// /*
-//  * check if null and check with database
-//  * 
-//  * */
-// if(checkEmpty($username,$password)){
-//     if(checkUser($username,$password)){
-//         $_SESSION['username']=$username;  //save user name
-//         if($autologin==1){                //if choose autologin, save cookies
-//             setcookie("username",$username,time()+3600*24*3);   //cookies valid date: 3 days
-//             setcookie("password",md5($password),time()+3600*24*3);
-//         }
-//         else{
-//             setcookie("username","",time()-1);   //if no auto login, clear cookie
-//             setcookie("password","",time()-1);
-//         }
-//         header("location: users.php ");            //if verified
-//     }
-// }
- 
- 
- 
-// //check empty
-// function checkEmpty($username,$password){
-//     if($username==null||$password==null){
-//         echo '<html><head><Script Language="JavaScript">alert("User name or password empty!");</Script></head></html>' . "<meta http-equiv=\"refresh\" content=\"0;url=login.html\">";
-//     }
-//     else{
-//             return true;
-//     }
-// }
- 
-
- 
-// //check whether username is in database
-// function checkUser($username,$password){
-//     $conn=new Mysql();
-//     $sql="select * from user where name='{$username}' and password='{$password}';";
-//     $result=$conn->sql($sql);
-//     if($result){
-//         return true;
-//     }
-//     else{
-//         echo '<html><head><Script Language="JavaScript">alert("User Not Exist");</Script></head></html>' . "<meta http-equiv=\"refresh\" content=\"0;url=login.html\">";
-//     }
-//     $conn->close();
-// }
- 
