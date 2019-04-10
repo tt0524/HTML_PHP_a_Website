7486bb2c6b7df00741eed706111480bce0d4be05
Xiaoting Jin <xajin@linkedin.com>
2019-04-09 23:31:31 -0700

    lab5

--- logout.php
+++ logout.php
@@ -2,6 +2,7 @@
 //log out and head to login page
 $_SESSION['username'] = null;
 unset($_SESSION['username']);
+session_destroy();
 setcookie("username","",time()-1);   //clear cookie
 setcookie("password","",time()-1);
 header("location: login.html ");
