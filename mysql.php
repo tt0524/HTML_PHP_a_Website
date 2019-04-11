b6f662129cca2d7471190356a428bc966b897312
Xiaoting Jin <xajin@linkedin.com>
2019-04-10 20:31:52 -0700

    mysql

--- mysql.php
+++ mysql.php
@@ -1,14 +1,15 @@
 <?php
 class Mysql{
          private static $host="localhost";
-         private static $user="root";
-         private static $password="";
+         private static $user="admin";
+         private static $password="12345678";
          private static $dbName="my_company";               //数据库名
          private static $charset="utf8";                    //字符编码
-         private static $port="3306";                       //端口号
+        // private static $port="3306";                       //端口号
          private  $conn=null;
          function __construct(){
-             $this->conn=new mysqli(self::$host,self::$user,self::$password,self::$dbName,self::$port);
+            // $this->conn=new mysqli(self::$host,self::$user,self::$password,self::$dbName,self::$port);
+             $this->conn=new mysqli(self::$host,self::$user,self::$password,self::$dbName);
              if(!$this->conn)
              {
                    die("Connection to Database Failed!".$this->conn->connect_error);
