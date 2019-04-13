af4ae69bc4ff886c6315818c2c9f91e1934b4794
Xiaoting Jin <xajin@linkedin.com>
2019-04-13 00:12:02 -0700

    update

--- search_user.php
+++ search_user.php
@@ -20,14 +20,43 @@ $cell_phone=$_POST['cell_phone'];
 
 $conn=new Mysql();
 
-$sql="SELECT * FROM my_customer WHERE ";
+$sql="SELECT * FROM my_customer WHERE
+			first_name  like ' %" . $first_name . "%'
+		and last_name  like ' %" . $last_name . "%'
+		and email  like ' %" . $email . "%'
+		and home_phone  like ' %" . $home_phone . "%'
+		and cell_phone  like ' %" . $cell_phone . "%'
+	";
  
 // excute query
 $result=$conn->sql($sql);
 
-// # ofrows affectd
-$num=$conn->getResultNum($sql);
+echo "<table border=\"1\" cellspacing=\"0\"
+        style=\"height: 90px; font-size: 10pt;\" cellpadding=\"10\">
+        <thead>
+        <tr>
+            <th style=\"width: 80px;\">ID</th>
+            <th style=\"width: 120px;\">First Name</th>
+            <th style=\"width: 120px;\">Last Name</th>
+            <th style=\"width: 220px;\">Email</th>
+            <th style=\"width: 220px;\">Address</th>
+            <th style=\"width: 220px;\">Home phone</th>
+            <th style=\"width: 220px;\">Cell phone</th>
+        </tr>
+        </thead>
+        <tbody>";
+                            
+/** Fetch each record in result set */
+for ($counter = 0; $row = mysqli_fetch_row($result); $counter++) {
+    echo "<tr>";
+    foreach ($row as $key => $value) {
+        echo "<td>$value</td>";
+    }
+    echo "<tr><br>";
+}
+echo "</tbody></table>";
+
 //close connection to DB
 $conn->close();
-die('New User Added');
-header("location: users.html ");
+
+
