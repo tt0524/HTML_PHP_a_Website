2734c3c7e2742fe6eb3ab41a01ff586d852bca7a
Xiaoting Jin <xajin@linkedin.com>
2019-04-13 01:11:22 -0700

    update

--- add_user.php
+++ add_user.php
@@ -1,37 +1,38 @@
 <?php
 require_once "mysql.php";
 
-// Check Empty
-if(!isset($_POST['first_name'])){
-    die('first_name is not define');
-    header("location: users.html ");
-}
-if(!isset($_POST['last_name'])){
-    die('age is not define');
-    header("location: users.html ");
-}
-if(!isset($_POST['email'])){
-    die('email is not define');
-    header("location: users.html ");
-}
-if(!isset($_POST['home_address'])){
-    die('address is not define');
-    header("location: users.html ");
-}
-if(!isset($_POST['home_phone'])){
-    die('home phone is not define');
-    header("location: users.html ");
-}
-if(!isset($_POST['cell_phone'])){
-    die('cell phone is not define');
-    header("location: users.html ");
-}
 $first_name=$_POST['first_name'];
 $last_name=$_POST['last_name'];
 $email=$_POST['email'];
 $address=$_POST['home_address'];
 $home_phone=$_POST['home_phone'];
 $cell_phone=$_POST['cell_phone'];
+
+// Check Empty
+if(!$first_name){
+    echo '<html><head><Script Language="JavaScript">alert("Empty First Name!");</Script></head></html>' . "<meta http-equiv=\"refresh\" content=\"0;url=users.html\">";
+    die();
+}
+if(!$last_name){
+    echo '<html><head><Script Language="JavaScript">alert("Empty Last Name!");</Script></head></html>' . "<meta http-equiv=\"refresh\" content=\"0;url=users.html\">";
+    die();
+}
+if(!$email){
+    echo '<html><head><Script Language="JavaScript">alert("Empty Email!");</Script></head></html>' . "<meta http-equiv=\"refresh\" content=\"0;url=users.html\">";
+    die();
+}
+if(!$address){
+    echo '<html><head><Script Language="JavaScript">alert("Empty Address!");</Script></head></html>' . "<meta http-equiv=\"refresh\" content=\"0;url=users.html\">";
+    die();
+}
+if(!$home_phone){
+    echo '<html><head><Script Language="JavaScript">alert("Empty Home Phone!");</Script></head></html>' . "<meta http-equiv=\"refresh\" content=\"0;url=users.html\">";
+    die();
+if(!$cell_phone){
+    echo '<html><head><Script Language="JavaScript">alert("Empty Cell Phone!");</Script></head></html>' . "<meta http-equiv=\"refresh\" content=\"0;url=users.html\">";
+    die();
+}
+
 
 // connect to DB
 
