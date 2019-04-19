b7a2bdf94b43b9d7627e8515c3fe510a40325110
Xiaoting Jin <southinsouth@gmail.com>
2019-04-19 16:24:51 -0700

    php

---
+++ untitled.php
@@ -0,0 +1,24 @@
+<?php
+$uri = $_SERVER['REQUEST_URI'];  
+$id = isset($_GET['id'])?$_GET['id']:0;
+ 
+if (!isset($_COOKIE['history'])) {           // if no product history
+    $his[] = $uri;                           // just store product uri
+ } else {                                    // if product history not empty
+    $his = explode('|',$_COOKIE['history']);  // explode
+    if (in_array($uri, $his)){
+        $pos = array_search($uri, $his);
+        $new_his = array_merge(array_slice($his, 0, $pos), array_slice($his, $pos));
+        $his = $new_his;
+        echo "YES!!";
+    }
+    array_unshift($his, $uri);
+    if (count($his) > 10) {             // store 10 url
+        array_pop($his);
+    }
+ }
+
+$expire=time()+60*60*24*30;
+setcookie('history',implode('|', $his),$expire);
+
+?>
