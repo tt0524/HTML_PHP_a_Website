58c7af427f222f14e90e7011c561faafbe3dd1c5
Xiaoting Jin <sharon.jin0524@gmail.com>
2019-04-21 00:33:30 -0700

    php count

---
+++ most_popular.php
@@ -0,0 +1,37 @@
+<?php
+
+if ( isset($_COOKIE['view_count']) ) {
+    $temp = explode('|',$_COOKIE['view_count']);
+    $prod = array_values(array_filter($temp, "even",ARRAY_FILTER_USE_KEY));
+    $count = array_values(array_filter($temp, "odd",ARRAY_FILTER_USE_KEY));
+    $view_count = cross_merge_array($prod, $count);
+    arsort($view_count);
+    $view_count_5 = array_slice($view_count, 0, 5);
+}
+
+
+
+function odd($var)
+{
+    return($var & 1);
+}
+
+function even($var)
+{
+
+    return(!($var & 1));
+}
+
+function cross_merge_array($arr1, $arr2)
+    {
+        $arr1 = array_values($arr1);
+        $arr2 = array_values($arr2);
+        $count = max(count($arr1), count($arr2));
+        $arr = array();
+        for ($i = 0; $i < $count; $i++) {
+            if ($i < count($arr1)) $arr[] = $arr1[$i]; // 判断，避免下标越界
+            if ($i < count($arr2)) $arr[] = $arr2[$i]; // 判断，避免下标越界
+        }
+        return $arr;
+    }
+?>
