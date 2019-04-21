6942b3d67764836325ba481b4574b89f93adf6b0
Xiaoting Jin <sharon.jin0524@gmail.com>
2019-04-21 00:04:20 -0700

    php count

--- cookie.php
+++ cookie.php
@@ -73,7 +73,7 @@ print_r("exist**");
 print_r(array_key_exists($uri, $prod));
 print_r("search**");
 print_r(array_search($uri, $prod));
-print_r("ISSET****")
+print_r("ISSET****");
 print_r(isset($prod[$uri]));
 
 function odd($var)
