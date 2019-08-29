80a96eeff64209122181804bebacda9f8a1beae5
Xiaoting Jin <sharon.jin0524@gmail.com>
2019-08-28 22:02:20 -0700

    first commit

--- database_operations.sql
+++ database_operations.sql
@@ -15,56 +15,55 @@ CREATE DATABASE my_company;
 USE my_company;
 
 -- create table
-DROP TABLE IF EXISTS my_customers;
-CREATE TABLE my_customers
+DROP TABLE IF EXISTS products;
+CREATE TABLE products
 (
 id              int primary key not null auto_increment,
-first_name      varchar(255) not null,
-last_name       varchar(255) not null,
-email           varchar(255) not null,
-home_address    text,
-home_phone      bigint not null,         
-cell_phone      bigint
+name      		varchar(255) not null,
+description     varchar(1000) not null,
+image_path      varchar(255),
+company         varchar(255) not null,
+url             varchar(255) not null
 );
 
 -- insert record
 INSERT INTO my_customers
 VALUES (1, 'Tianyu', 'Cao', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (2, 'Yuxiang', 'Chen', 'cao@gmail.com', '122 Sunnyvale', 1234556678,1234156678);
+VALUES (2, 'Wenxiag', 'Hu', 'hu@gmail.com', '122 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (3, 'Chikei', 'Loi', 'cao@gmail.com', '141 Sunnyvale', 1234556678,1234156678);
+VALUES (3, 'Chikei', 'Loi', 'Chikei@gmail.com', '141 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (4, 'Gaochao', 'Wang', 'cao@gmail.com', '421 Sunnyvale', 1234556678,1234156678);
+VALUES (4, 'Gaochao', 'Wang', 'gao@gmail.com', '421 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (5, 'Xiaoting', 'Jin', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (5, 'Xiaoting', 'Jin', 'xiaoting@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (6, 'Emily', 'Wu', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (6, 'Emily', 'Wu', 'emily@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (7, 'Lily', 'Sanford', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (7, 'Lily', 'Sanford', 'lily@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (8, 'Tony', 'Lim', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (8, 'Tony', 'Lim', 'tony@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (9, 'Mark', 'Su', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (9, 'Mark', 'Su', 'mark@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (10, 'Mike', 'Ye', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (10, 'Mike', 'Ye', 'mike@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (11, 'Faro', 'Wu', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (11, 'Faro', 'Wu', 'faro@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (12, 'Azakaban', 'Wang', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (12, 'Azakaban', 'Wang', 'Azakaban@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (13, 'Holdem', 'Kim', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (13, 'Holdem', 'Kim', 'holdem@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (14, 'Cloud', 'Xie', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (14, 'Cloud', 'Xie', 'cloud@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (15, 'Potter', 'Me', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (15, 'Potter', 'Me', 'potter@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (16, 'Snape', 'Cao', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (16, 'Snape', 'Cao', 'snape@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (17, 'Kitty', 'Cao', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (17, 'Kitty', 'Cao', 'kitty@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (18, 'Jessie', 'Cao', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (18, 'Jessie', 'Cao', 'jessie@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (19, 'Ryan', 'Cao', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (19, 'Ryan', 'Cao', 'ryan@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
 INSERT INTO my_customers
-VALUES (20, 'Ben', 'Cao', 'cao@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
+VALUES (20, 'Ben', 'Cao', 'ben@gmail.com', '121 Sunnyvale', 1234556678,1234156678);
